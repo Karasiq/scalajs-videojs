110eceb3a4e003257e473c91343ccf7646b49cab
Scala Steward <scala_steward@virtuslab.com>
2023-10-03 21:49:11 +0000

    Update sbt-scalajs to 1.14.0

--- project/project/ProjectDefs.scala
+++ project/project/ProjectDefs.scala
@@ -1,7 +1,7 @@
 //noinspection DuplicatedCode,ScalaPackageName
 
 object ProjectDefs {
-  val ScalaJSVersion: String = sys.props.getOrElse("SCALAJS_VERSION", "0.6.33")
+  val ScalaJSVersion: String = sys.props.getOrElse("SCALAJS_VERSION", "1.14.0")
 
   def scalaJSIs06: Boolean =
     ScalaJSVersion.startsWith("0.6.")
